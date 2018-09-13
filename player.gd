60e54143d3656dd97e6836c5e8fb7330dbc7627d
Devlin McClure <mccluredt@gmail.com>
2018-09-13 11:04:33 -0400

    Change player to Area2D and create player_2

--- player.gd
+++ player.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends Area2D
 
 # class member variables go here, for example:
 # var a = 2
@@ -15,29 +15,33 @@ func _ready():
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
-#Get player input for change in movement vector
-func get_input():
-	#set the player velocity every physics update
-	velocity = Vector2()
-	
-	#Player moves up
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
 	if Input.is_action_pressed("player_1_up"):
-		velocity.y = -1
-	#Player moves down
+		self.position.y -= speed *delta
 	if Input.is_action_pressed("player_1_down"):
-		velocity.y = 1
-	
-	#Player speed is calculated by velocity multipled by speed
-	velocity = velocity * speed
-	
-#Player movement is processed here
-func _physics_process(delta):
-	#Get the input from the player
-	get_input()
-	#Calculate the player velocity from the vector set by player input
-	move_and_slide(velocity)
+		self.position.y += speed *delta
+	pass
+
+##Get player input for change in movement vector
+#func get_input():
+#	#set the player velocity every physics update
+#	velocity = Vector2()
+#
+#	#Player moves up
+#	if Input.is_action_pressed("player_1_up"):
+#		velocity.y = -1
+#	#Player moves down
+#	if Input.is_action_pressed("player_1_down"):
+#		velocity.y = 1
+#
+#	#Player speed is calculated by velocity multipled by speed
+#	velocity = velocity * speed
+#
+##Player movement is processed here
+#func _physics_process(delta):
+#	#Get the input from the player
+#	get_input()
+#	#Calculate the player velocity from the vector set by player input
+#	move_and_slide(velocity)
