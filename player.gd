fdf19b3d76bb1bc2a443a2d0f7183cd30d29e2b2
Devlin McClure <mccluredt@gmail.com>
2018-09-20 11:15:15 -0400

    Add movement keys for players 3 and 4

--- player.gd
+++ player.gd
@@ -24,25 +24,3 @@ func _process(delta):
 	if Input.is_action_pressed(which+"_down"):
 		self.position.y += speed *delta
 	pass
-
-##Get player input for change in movement vector
-#func get_input():
-#	#set the player velocity every physics update
-#	velocity = Vector2()
-#
-#	#Player moves up
-#	if Input.is_action_pressed("player_1_up"):
-#		velocity.y = -1
-#	#Player moves down
-#	if Input.is_action_pressed("player_1_down"):
-#		velocity.y = 1
-#
-#	#Player speed is calculated by velocity multipled by speed
-#	velocity = velocity * speed
-#
-##Player movement is processed here
-#func _physics_process(delta):
-#	#Get the input from the player
-#	get_input()
-#	#Calculate the player velocity from the vector set by player input
-#	move_and_slide(velocity)
