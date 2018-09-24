f333821e0085364580ca01104c4c1e6def229fb1
Devlin McClure <mccluredt@gmail.com>
2018-09-24 16:59:13 -0400

    Give players new graphics and give each player a number in code

--- player.gd
+++ player.gd
@@ -13,10 +13,8 @@ export var speed = 1
 #Direction in which ball moves
 export var ball_dir = 1
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+#Player identification for score
+export var player_number = 1
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
@@ -26,11 +24,8 @@ func _process(delta):
 		self.position.y -= speed *delta
 	if Input.is_action_pressed(which+"_down"):
 		self.position.y += speed *delta
-	pass
 
 func _on_area_entered(area):
 	if area.get_name() == "ball":
 		# assign new direction
 		area.direction = Vector2(ball_dir, randf() * 2 - 1).normalized()
-
-	pass # replace with function body
